3544e393da51e1e0e2fd9d89f816e49c845363db
sainsh <kfsainsh@gmail.com>
2018-09-20 13:01:55 +0200

    classroom

---
+++ src/Classroom/Main.java
@@ -0,0 +1,23 @@
+package Classroom;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("input radius of circle");
+        double radius = in.nextInt();
+        System.out.println("radius: " + radius);
+
+        System.out.println("calculating area...");
+
+        double area = radius * radius * Math.PI;
+
+        System.out.println("The area of the circle of radius: " +radius + " is " + area);
+
+    }
+
+}
