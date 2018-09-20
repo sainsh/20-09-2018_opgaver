93d9869bbdb4ac8810d68cddc7d07936c19472a5
sainsh <kfsainsh@gmail.com>
2018-09-20 11:31:52 +0200

    ninth

---
+++ src/ninth/Main.java
@@ -0,0 +1,16 @@
+package ninth;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double x = 5.3;
+        double y = 8.6;
+        double area= x*y;
+        double perimeter = 2*(x+y);
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: "+ perimeter);
+
+    }
+}
