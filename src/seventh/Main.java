4fb52a82c621bfffa354c4252d59393064613051
sainsh <kfsainsh@gmail.com>
2018-09-20 11:22:47 +0200

    seventh

---
+++ src/seventh/Main.java
@@ -0,0 +1,13 @@
+package seventh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double x;
+        x=4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11));
+        System.out.println(x);
+        x=4*(1.0-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11+1.0/13);
+        System.out.println(x);
+    }
+
+}
