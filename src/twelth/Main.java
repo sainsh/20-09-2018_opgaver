5004765cac19da3354430b54d538c92e18667043
sainsh <kfsainsh@gmail.com>
2018-09-20 12:05:52 +0200

    twelth

---
+++ src/twelth/Main.java
@@ -0,0 +1,16 @@
+package twelth;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double distance = 24/1.6;
+
+        double time = 1 + 40.0/60 + 35.0/60/60;
+
+        double speed = distance/time;
+
+        System.out.println(speed);
+
+    }
+}
