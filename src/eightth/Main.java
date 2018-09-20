6580f04d198c43da8c9ef192f0a08cb21985976c
sainsh <kfsainsh@gmail.com>
2018-09-20 11:28:36 +0200

    eightth

---
+++ src/eightth/Main.java
@@ -0,0 +1,19 @@
+package eightth;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double phi = 3.14159;
+        double radius = 6.5;
+        double perimeter = 2*radius*phi;
+        double area = radius*radius*phi;
+
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+
+
+
+    }
+}
