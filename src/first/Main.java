52eab49e5c926134bede31a9f77c512b9a6a9d09
sainsh <kfsainsh@gmail.com>
2018-09-20 10:47:01 +0200

    first + second

---
+++ src/first/Main.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java");
+        System.out.println("Learning java Now");
+        System.out.println("Programming is fun");
+
+    }
+
+}
---
+++ src/second/Main.java
@@ -0,0 +1,12 @@
+package second;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i =0; i<5; i++){
+            System.out.println("I Love Java");
+        }
+
+    }
+}
