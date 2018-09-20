d0065194fe3366cf9597ae8700d4362565a5e300
sainsh <kfsainsh@gmail.com>
2018-09-20 12:30:09 +0200

    thirteenth

---
+++ src/thirteenth/Main.java
@@ -0,0 +1,15 @@
+package thirteenth;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double y = -0.31351;
+        double x = 38.19*y +29.69;
+
+
+        System.out.println("x: " + x + " y: " + y);
+
+
+    }
+}
