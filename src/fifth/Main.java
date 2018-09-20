74799b27ecd43e8596055a359f25ec9c5f85e401
sainsh <kfsainsh@gmail.com>
2018-09-20 11:10:44 +0200

    fifth

---
+++ src/fifth/Main.java
@@ -0,0 +1,10 @@
+package fifth;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double x = ((7.5*6.5)-(4.5*3))/(47.5-5.5);
+        System.out.println(x);
+    }
+
+}
