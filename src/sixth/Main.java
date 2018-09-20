b78e0f9083b9fe47dbd50ced99d07e7882907dfa
sainsh <kfsainsh@gmail.com>
2018-09-20 11:14:46 +0200

    sixth

---
+++ src/sixth/Main.java
@@ -0,0 +1,17 @@
+package sixth;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 0;
+
+        for(int n=1; n<11; n++){
+
+            result += n;
+
+        }
+
+        System.out.println(result);
+    }
+}
